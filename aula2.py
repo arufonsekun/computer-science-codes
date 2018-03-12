fe1c39492f164f5b011e2604ccb3eff14d0847b6
aluno-uffs <Aluno UFFS>
2018-03-12 15:54:27 -0300

    Exercicios da lista

--- aula2.py
+++ aula2.py
@@ -3,6 +3,7 @@ def mediaFinal(np, nt):
     print("Sua media eh", int(np)*0.7 + int(nt)*0.3)
 #mediaFinal(input("Digite sua nota da prova: "), input("Digite sua nota do trab: "))
 
+#Resolve baskara
 def segundoGrau():
     a = int(input('a: '))
     b = int(input('b: '))
@@ -14,4 +15,12 @@ def segundoGrau():
         print("As raizaes sao",raiz1, raiz2)
     except ValueError:
         print ("A raiz eh negativa: ",delta)
-segundoGrau()
+#segundoGrau()
+
+#Exercicio 1 produro de 28 por 43
+print ("O produto eh", 28 * 43)
+
+#Exercicio 2
+x = int(input("Digite o 1º numero: "))
+y = int(input("Digite o 2º numero: "))
+print ("A soma eh ", x+y)
