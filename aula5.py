b5d3f4f8371cdaeccf3d3b4485614e6b0af981c2
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2018-04-01 14:53:46 -0300

    Update aula5.py

--- aula5.py
+++ aula5.py
@@ -24,16 +24,28 @@ def soma():
         soma += 1 / count
         count += 1
     print (soma)
-
+#Exercício 1
 def paresImpares():
     num = int(input("Digite um numero: "))
     par = 0
     odd = 0
     while num != 0:
-        if num % 2 == 0:par += 1
-        else: odd += 1
+        if num % 2 == 0:par += num
+        else: odd += num
         num = int(input("Digite um numero: "))
-    print ("O n° de pares é {0} e impares é {1}".format(par, odd))
+    print ("A soma dos pares é {0} e impares é {1}".format(par, odd))
+#Exercício 2
+def antecessor_sucessor():
+    num = int()
+    count = 0
+    white count < 10:
+        num = int(input("Digite um numero:"))
+        print("O antecessor é {0} e o sucessor é {1}".format(num - 1, num + 1))
+        count += 1
+#Exercício 3
+def reinventar_a_roda():
+    print(1)
+   
 
 #senha()
 #mediaNotas()
