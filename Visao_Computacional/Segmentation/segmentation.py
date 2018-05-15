2d22105fc1b86cd53e81cb7e1c84725e96c23213
junior-16 <junior.ramisch@gmail.com>
2018-05-15 04:03:58 -0300

    vai se fude

---
+++ Visao_Computacional/Segmentation/segmentation.py
@@ -0,0 +1,29 @@
+#I took this code in here:https://goo.gl/nk65md
+import cv2
+#import numpy as np
+from matplotlib import pyplot as plt
+import sys
+# loading image
+#img0 = cv2.imread('SanFrancisco.jpg',)
+img0 = cv2.imread(sys.argv[1])
+
+# converting to gray scale
+gray = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)
+# remove noise
+img = cv2.GaussianBlur(gray,(3,3),0)
+
+# convolute with proper kernels
+laplacian = cv2.Laplacian(img,cv2.CV_64F)
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)  # x
+sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)  # y
+
+plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
+plt.title('Original'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
+plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
+plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
+plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
+plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+
+plt.show()
