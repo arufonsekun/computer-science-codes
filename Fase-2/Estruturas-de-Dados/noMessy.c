99c4ac038b1880cb801330720417dd7886e87067
junior-16 <junior.ramisch@gmail.com>
2018-08-15 19:47:20 -0300

    data structure task done

--- Fase-2/Estruturas-de-Dados/noMessy.c
+++ Fase-2/Estruturas-de-Dados/noMessy.c
@@ -1,14 +1,41 @@
-//iplementar o bagulho sem gambiarra
+//implementar o bagulho sem gambiarra
 #include <stdio.h>
 
 #define MAXJ 20
 #define MAXI 12
 
-int main(){
-    struct sales = {
-        unsigned int i;
-        float moths[MAXI];
-    }
+struct sales{
+    char code[4];
+    float months[MAXI];
+};
 
+struct sales control[MAXJ];
+
+void print(){
+    printf("Cod\tJan\tFev\tMar\tAbr\tMai\tJun\tJul\tAgo\tSet\tOut\tNov\tDez\n");
+    for (unsigned int i = 0; i < MAXJ; i++)
+    {
+        printf("%s\t", control[i].code);
+        for (unsigned int j = 0; j < MAXI; j++)
+        {
+            printf("%.1f\t", control[i].months[j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+
+    struct sales product;
+    for(unsigned int i = 0; i < MAXJ; i++){
+
+        scanf("%s\n", product.code);
+
+        for (unsigned int j = 0; j < MAXI; j++){
+            scanf("%f\n", &product.months[j]);
+        }
+        control[i] = product;
+    }
+    print();
     return 0;
 }
