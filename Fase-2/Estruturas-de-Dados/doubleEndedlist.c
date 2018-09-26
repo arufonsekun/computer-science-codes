ba6241f43c0ce87b7774e53228a682ac6fa3c05b
junior-16 <junior.ramisch@gmail.com>
2018-09-26 02:39:41 -0300

    task done, missing comments and some gambiarra fixes

--- Fase-2/Estruturas-de-Dados/doubleEndedlist.c
+++ Fase-2/Estruturas-de-Dados/doubleEndedlist.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//insert before-> (value1, value2)
-//insert index(value) -1
-
 struct vector{
     int value;
     struct vector * next;
@@ -32,26 +29,26 @@ void push_back(int value, vector ** list){
 void print(vector * list){
     int counter = 0;
     while (list != NULL){
-        printf("vector [%d] = %d\n", counter, list->value);
+        printf("vector[%d] = %d\n", counter, list->value);
         counter++;
         list = list->next;
     }
 }
 
 void printBackwards(vector * tail){
+
     int index = 0;
     while (tail != NULL){
-        if (tail->next != NULL)
-            printf("vector[%d] = %d\n", index, tail->value);
-        
+        printf("vector[%d] = %d\n", index, tail->value);
         tail = tail->previous;
         index++;
        
     }
+
 }
 
 //delete an element given a value
-void delete(int value, vector ** head){
+vector * delete(int value, vector ** head){
     vector * aux = *head;
     vector * auxHead = *head;
 
@@ -62,16 +59,10 @@ void delete(int value, vector ** head){
     }
 
     else{
-        aux = auxHead;
 
-        while (auxHead != NULL){
-            //remove the last value (gambiarra alert!!!!)
-            if (auxHead->value == value & auxHead->next == NULL){
-                aux->next = NULL;
-                free(auxHead);
-            }
+        while (auxHead->next != NULL){
 
-            else if (auxHead->value == value){
+            if (auxHead->value == value){
                 aux->next = auxHead->next;
                 aux->next->previous = aux;
                 free(auxHead);
@@ -80,35 +71,87 @@ void delete(int value, vector ** head){
             aux = auxHead;
             auxHead = auxHead->next;
         }
-    }
 
+        if ((auxHead->value == value) & (auxHead->next == NULL)){
+            aux->next = NULL;
+            free(auxHead);
+            return aux;
+        }
+        else{
+            return auxHead;
+        }
+    }
     
 }
 
+void insertBefore(int new, int before, vector ** head){
+    vector * newValue = (vector *) malloc (sizeof(vector *));
+    newValue->value = new;
+
+    if ((*head)->value == before){
+        newValue->previous = NULL;
+        newValue->next = *head;
+        (*head)->previous = newValue;
+        *head = newValue;
+    }
+    else{
+        vector * behind = *head;
+        vector * front = (*head)->next;
+        while (front != NULL){
+            if (front->value == before){
+                newValue->next = front;
+                newValue->previous = behind;
+                behind->next = newValue;
+                front->previous = newValue;
+            }
+            behind = front;
+            front = front->next;
+        }
+    }
+}
+
 int main(){
-    vector * list = NULL, * head;
-    int input, del;
+    //tail era pra ser list mas como list apontava pro fim agora vai ser tail o nome e fodasse
+    vector * tail = NULL, * head;
+    int operation = 1, input, before;
     int counter = 0;
     
-    scanf("%d", &input);
-    while (input != 0){
-        push_back(input, &list);
-        if (counter == 0){
-            head = list;
-            counter = 1;
-        }
-        scanf("%d", &input);
-    }
+    while (operation != 0){
+        
+        printf("\n0-Leave \n1-Insert \n2-Remove \n3-List Normal \n4-List Backwards \n5-Insert Before \n6-Clear terminal");
+        printf("Type the operation code: ");
+        scanf("%d", &operation);
 
-    printf("Type a value o remove: ");
-    scanf("%d", &del);
-    delete(del, &head);
-    
-    printf("Print backwards\n");
-    printBackwards(list);
-    
-    printf("Print normal\n");
-    print(head);
+        if (operation == 1){
+            printf("Type the value to insert: ");
+            scanf("%d", &input);
+            push_back(input, &tail);
+           
+            if (counter == 0){
+                head = tail;
+                counter = 1;
+            }
+        }
+
+        else if (operation == 2){
+            printf("Type the value to remove: ");
+            scanf("%d", &input);
+            tail=delete(input, &head);
+        }
+
+        else if (operation == 3)
+            print(head);
+        else if (operation == 4)
+            printBackwards(tail);
+        else if (operation == 5){
+            printf("Type the value to insert followed by the existing value: ");
+            scanf("%d %d", &input, &before);
+            insertBefore(input, before, &head);
+        }
+        else
+            system("clear");
+
+    }
 
     return 0;
 }
