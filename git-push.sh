7830e9db69743c19b7037141ada77170de87eac5
junior-16 <junior.ramisch@gmail.com>
2018-11-11 17:53:14 -0200

    decade counter

--- git-push.sh
+++ git-push.sh
@@ -5,7 +5,7 @@ if [ $yes = "Y" ]; then
     git add .
     echo "Type the commit message"
     read message
-    git commit -m $message
+    git commit -m "$message"
     git push
 else
     echo "Fuck you"
