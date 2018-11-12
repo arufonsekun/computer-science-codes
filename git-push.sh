4d26e8e66c70ee220fd42ad7ab03edd4e58e3736
junior-16 <junior.ramisch@gmail.com>
2018-11-11 23:45:15 -0200

    changing git-push script

--- git-push.sh
+++ git-push.sh
@@ -2,11 +2,13 @@
 echo "Are you sure, you wanna push? (Y, N)"
 read yes
 if [ $yes = "Y" ]; then
+    echo "STAGGING FILES"
     git add .
-    echo "Type the commit message"
+    echo "Type the commit message: "
     read message
     git commit -m "$message"
+    echo "PUSHING TO MASTER"
     git push
 else
-    echo "Fuck you"
+    echo "Fuck you!!"
 fi
