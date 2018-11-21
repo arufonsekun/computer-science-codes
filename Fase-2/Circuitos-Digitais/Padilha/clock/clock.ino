414b12a46e202d8677846e503439a554aea25733
aluno-uffs <Aluno UFFS>
2018-11-21 15:06:12 -0200

    change clock.ino

--- Fase-2/Circuitos-Digitais/Padilha/clock/clock.ino
+++ Fase-2/Circuitos-Digitais/Padilha/clock/clock.ino
@@ -1,15 +1,27 @@
 //essa eh a merda de um oscilador, vulgo clock
 void setup() {
   // put your setup code here, to run once:
-  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(7, INPUT);
 
 }
 
 void loop() {
-  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
   delay(2000);
+  digitalWrite(11, LOW);
+  delay(2000);
+  
+  digitalWrite(8, HIGH);
+  delay(1000);
+  digitalWrite(8, LOW);
+  delay(1000);
+  
+  digitalWrite(2, HIGH);
+  delay(500);
   digitalWrite(10, LOW);
-  delay(2000);
-  // put your main code here, to run repeatedly:
+  delay(500);
 
 }
