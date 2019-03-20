32f83591ec49cd83f8ed629ac07f38b5927be455
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2019-03-20 11:38:42 -0300

    encapsulation lesson

--- Fase-3/Programacao-I/Main.java
+++ Fase-3/Programacao-I/Main.java
@@ -1,23 +1,12 @@
 class Main{
-    public static void main(String args[]){
-      Data a, b;
-      a = new Data();
-      b = new Data();
+    public static void main(String[] args){
+        Retangle r = new Retangle();
+        r.setHeight(3.14);
+        r.setBase(5.6);
+        r.setAge(12);
 
-      a.setDia(23);
-      a.setMes(3);
-
-      b.setDia(20);
-      b.setMes(6);
-
-      System.out.println("Dia: "+a.getDia() + " " + "Mes: "+a.getMes());
-
-      Person m;
-      m = new Person();
-      m.age = 18;
-      m.birth = b;
-      m.height = 1.78f;
-
-      System.out.println("Age: "+m.age+ " " + " Birth: "+m.birth.getDia()+" Day: "+m.birth.getMes() + " Height: " + m.height);
+        System.out.println("Altura "+r.getHeight());
+        System.out.println("Base "+r.getBase());
+        System.out.println("Idade "+r.getAge());
     }
 }
