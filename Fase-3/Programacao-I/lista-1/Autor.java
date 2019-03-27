3dc9d77c7b287afb0f03acf253a559b5e9a8bb4e
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2019-03-27 10:05:56 -0300

    creating a constructor to Autor class

--- Fase-3/Programacao-I/lista-1/Autor.java
+++ Fase-3/Programacao-I/lista-1/Autor.java
@@ -1,12 +1,17 @@
 class Autor{
     private String nome;
     private int id;
+    
+    public void Autor(String n, int i){
+        this.setId(i);
+        this.setNome(n);
+    }
 
-    public void setId(int id){
+    private void setId(int id){
         this.id = id;
     }
 
-    public void setNome(String nome){
+    private void setNome(String nome){
         this.nome = nome;
     }
 
