04cc599f204541d2980df4cbb380cad735773b05
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2019-04-02 20:48:12 -0300

    there is a bug on query function after the update

---
+++ Fase-3/EDII/seg-tree/dangeroPaths.cc
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <bitset>
+using namespace std;
+
+typedef tuple<int,int,int> iii;
+
+vector<int> monsters;
+vector<iii> tree;
+
+int query(int b, int e, int i = 1){
+    if (get<0>(tree[i]) == get<1>(tree[i]))
+        return get<2>(tree[i]);
+
+	if (get<0>(tree[i]) == b && get<1>(tree[i]) == e)
+		return get<2>(tree[i]);
+
+    if (e < get<0>(tree[i])) return 0;
+    if (get<1>(tree[i]) < b) return 0;
+
+    int c = query(b, e, i*2);
+    int d = query(b, e, i*2 + 1);
+    return (c | d);
+}
+
+int update(int cor, int mN, int i = 1){
+	if (get<0>(tree[i]) == cor && get<1>(tree[i]) == cor){
+		get<2>(tree[i]) = 1 << (mN - 1);
+		return get<2>(tree[i]);
+	}
+	if (get<0>(tree[i]) == get<1>(tree[i]))
+        return get<2>(tree[i]);
+
+	float miau = (get<0>(tree[i]) + get<1>(tree[i])) / 2;
+	if (cor <= miau){
+		return get<2>(tree[i]) = get<2>(tree[i*2 +1]) | update(cor, mN, i*2);
+	}
+	else
+		return get<2>(tree[i]) = (get<2>(tree[i*2]) | update(cor, mN, i*2 +1));
+
+	return 0;//return 0 just in case the shit happen
+}
+
+int fill(int b, int e, int i = 1){
+    get<0>(tree[i]) = b;
+    get<1>(tree[i]) = e;
+    if (b == e){
+        return get<2>(tree[i]) = monsters[b-1];
+    }
+
+    int ql = fill(b, (b+e)/2, i*2);
+    int qr = fill(((b+e)/2)+1, e, i*2 + 1);
+    return get<2>(tree[i]) = ql | qr;
+}
+
+int main(){
+    int n, q, m;
+    int o, b, e;// o eh a operacao 1 ou 2
+    int monN;// n° do mostru
+    iii no = {0,0,0};// n from tree node
+
+    cin >> n >> q >> m;
+    monsters.assign(n, 0);
+    tree.assign(n*2+1, no);
+
+    for (int i = 0; i < m; i++){
+        cin >> monN;
+        monsters.at(i) = 1 << (monN -1);
+    }
+
+    fill(1, n);
+	cout << bitset<5>(query(2,5)) << endl;
+	update(3, 3);
+	cout << bitset<5>(query(1,2)) << endl;
+
+	//cout << bitset<5>(get<2>(tree[4])) << endl;
+
+    // for (unsigned int i = 1; i < tree.size(); i++){
+    //     cout << "b = " << get<0>(tree[i]);
+    //     cout << " e = " << get<1>(tree[i]);
+    //     cout << " q = " << bitset<5>(get<2>(tree[i])) << endl;
+    // }
+
+   // for (int i = 0; i < q; i++){
+   //     cin >> o >> b >> e;
+   //     if (o == 1) cout << bitset<5>(query(b, e)) << endl;
+   //     else update(b, e);
+   // }
+
+    return 0;
+}
