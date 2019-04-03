392f8ba9d87d8a88d52c4d64c758f29a5ee73c7e
Junior-16 <junior.ramisch@gmail.com>
2019-04-03 00:25:56 -0300

    quick sort tem um problema

--- Fase-3/EDII/sort-algorithms/quick-sort.py
+++ Fase-3/EDII/sort-algorithms/quick-sort.py
@@ -2,23 +2,42 @@ import random
 v = list(int (x) for x in input().split())
 n = len(v)
 #pivo = random.choice(range(0, n))
-pivo = int(input("Digite a posição do pivo: "))
+pivo = -1
 l = 0; r = n-1
 turn = 0 #0 means comparison with left and 1 with right
-while l != r:
-    if not turn:
-        if pivo == l: turn = 1
-        if v[pivo] < v[l]:
-            v[l], v[pivo] = v[pivo], v[l]
-            pivo = l
-            turn = 1
-        l+=1
-    if turn:
-        if pivo == r: turn = 0
-        if v[pivo] > v[r]:
-            v[r], v[pivo] = v[pivo], v[r]
-            pivo = r
-            turn = 0
-        r-=1
-    print(v)
+counter = 0
+pivos = list()
 
+def escolhePivo(p):
+    if p == -1:
+        k = random.choice(range(0, n))
+        pivos.append(k)
+        return k
+    else:
+        pN = random.choice(range(0, n))
+        while pN in pivos:
+            pN = random.choice(range(0, n))
+        pivos.append(pN)
+        return pN
+
+while counter < n:
+    pivo = escolhePivo(pivo)
+    print(pivo)
+    while l != r:
+        if not turn:
+            if pivo == l: turn = 1
+            if v[pivo] < v[l]:
+                v[l], v[pivo] = v[pivo], v[l]
+                pivo = l
+                turn = 1
+            l+=1
+        if turn:
+            if pivo == r: turn = 0
+            if v[pivo] > v[r]:
+                v[r], v[pivo] = v[pivo], v[r]
+                pivo = r
+                turn = 0
+            r-=1
+        print(v)
+    l = 0; r = n-1; turn = 0
+    counter += 1
