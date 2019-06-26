6210a5b503a17ddd720f1f1c1adf31f023021384
Junior Vitor Ramisch <junior.ramisch@gmail.com>
2019-06-26 10:51:54 -0300

    divisor testado e funcionando

--- Fase-3/Sistemas-Digitais/NP1/divisor_8bits.vhdl
+++ Fase-3/Sistemas-Digitais/NP1/divisor_8bits.vhdl
@@ -2,27 +2,17 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.STD_LOGIC_unsigned.all;
 
-entity divisor_8bits is
+entity divisor is
     port(
         --numerator
-        a0 : in std_logic;
-        a1 : in std_logic;
-        a2 : in std_logic;
-        a3 : in std_logic;
-        a4 : in std_logic;
-        a5 : in std_logic;
-        a6 : in std_logic;
-        a7 : in std_logic;
-
-        --denominator
-        b0 : in std_logic;
-        b1 : in std_logic;
-        b2 : in std_logic;
-        b3 : in std_logic;
-        b4 : in std_logic;
-        b5 : in std_logic;
-        b6 : in std_logic;
-        b7 : in std_logic;
+        i0 : in std_logic;
+        i1 : in std_logic;
+        i2 : in std_logic;
+        i3 : in std_logic;
+        i4 : in std_logic;
+        i5 : in std_logic;
+        i6 : in std_logic;
+        i7 : in std_logic;
 
         --quotient
         o0 : out std_logic;
@@ -35,14 +25,14 @@ entity divisor_8bits is
         o7 : out std_logic
 
     );
-end divisor_8bits;
+end divisor;
 
-architecture behavior of divisor_8bits is
+architecture behavior of divisor is
 
     procedure div8(
         numer : in std_logic_vector(7 downto 0);
         denom : in std_logic_vector(7 downto 0);
-        quotient : std_logic_vector(7 downto 0)) is
+        quotient : out std_logic_vector(7 downto 0)) is
 
     variable d, n1 : std_logic_vector(8 downto 0);
     variable n2 : std_logic_vector(7 downto 0);
@@ -50,7 +40,7 @@ architecture behavior of divisor_8bits is
     begin
         d := '0' & denom;
         n2 := numer;
-        n1 := '0' & numer;
+        n1 := "000000000";
         for i in 0 to 7 loop
             n1 := n1(7 downto 0) & n2(7);
             n2 := n2(6 downto 0) & '0';
@@ -65,13 +55,12 @@ architecture behavior of divisor_8bits is
 
     begin --architecture
 
-        process(a0,a1,a2,a3,a4,a5,a6,a7,b0,b1,b2,b3,b4,b5,b6,b7)
+        process(i0,i1,i2,i3,i4,i5,i6,i7)
             variable output : std_logic_vector(7 downto 0);
             begin
-                div8(a7 & a6 & a5 & a4 & a3 & a2 & a1 & a0, b7 & b6 & b5 & b4 & b3 & b2 & b1 & b0, output);
-                o7 < output(7); o6 < output(6); o5 < output(5); o4 < output(4);
-                o3 < output(3); o2 < output(2); o1 < output(1); o0 < output(0);
+                div8(i7 & i6 & i5 & i4 & i3 & i2 & i1 & i0, "10000000", output);
+                o7 <= output(7); o6 <= output(6); o5 <= output(5); o4 <= output(4);
+                o3 <= output(3); o2 <= output(2); o1 <= output(1); o0 <= output(0);
         end process;
 
     end behavior;
-            
