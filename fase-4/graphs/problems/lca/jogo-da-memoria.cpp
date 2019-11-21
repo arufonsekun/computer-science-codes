a75e6b3c5f8c4e0ea736412b3e6de370beb566bd
arufonsekun <junior.ramisch@gmail.com>
2019-11-21 01:00:36 -0300

    associação no <---> altura está funcionando

--- fase-4/graphs/problems/lca/jogo-da-memoria.cpp
+++ fase-4/graphs/problems/lca/jogo-da-memoria.cpp
@@ -14,6 +14,7 @@ vector<bool> visited;
 vector<int> euler_tour_vertices;
 vector<int> euler_tour_dist;
 vector<tiii> segment_tree;
+vector<int> card_order;
 
 void print(vector<vector<int> > tree) {
 
@@ -39,7 +40,7 @@ void computeHeight(vector<vector<int>> tree, int parent) {
 void euler_tour(vector<vector<int>> tree, int parent, int current) {
 
     visited.at(current) = true;
-    euler_tour_dist.push_back(height[current-1]);
+    euler_tour_dist.push_back(height[current]);
     euler_tour_vertices.push_back(current);
 
     for (int adj_vertex :  tree.at(current)) {
@@ -48,12 +49,11 @@ void euler_tour(vector<vector<int>> tree, int parent, int current) {
         }
     }
 
-    if (parent != -1){
+    if (parent != current) {
         visited.at(parent) = true;
         euler_tour_dist.push_back(height[parent]);
         euler_tour_vertices.push_back(parent);
     }
-        
 }
 
 /*int fill_seg_tree(int left, int right, int i = 1) {
@@ -65,15 +65,25 @@ void euler_tour(vector<vector<int>> tree, int parent, int current) {
     return get<2>(segment_tree[i]) = min( fill_seg_tree(left, (left + right) / 2, i*2), fill_seg_tree((left + right) / 2 + 1, right, i*2+1) );
 }*/
 
+/*
+void dfs(vector<vector<int>> tree, int parent) {
+    visited.at(parent) = true;
+    for(int adj_vertex : tree[parent]) {
+        if (!visited.at(adj_vertex)) {
+            cout << "Node: " << card_order.at(adj_vertex) << " Height: " << height.at(adj_vertex) << endl;
+            dfs(tree, adj_vertex);
+        }
+    }
+}
+*/
+
 int main(){
 
-    vector<int> card_order;
     int v1, v2, card_number;
     tiii seg = {0, 0, 0};
 
     cin >> n_cards;
 
-    visited.assign(n_cards, false);
     height.assign(n_cards, 0);
     vector<vector<int>> tree(n_cards);
 
@@ -89,18 +99,24 @@ int main(){
 	    tree[v2].push_back(v1);
     }
 
+    /*
+        DFS
+        visited.assign(n_cards, false);
+        cout << "Node: " << card_order.at(0) << " Height: " << height.at(0) << endl;
+        dfs(tree, 0);
+    */
 
+    visited.assign(n_cards, false);
     height.at(0) = 0;
     computeHeight(tree, 0);
 
     visited.assign(n_cards, false);
-
     visited.at(0) = true;
-    euler_tour(tree, tree[0][0], tree[0][0]);
+    euler_tour(tree, 0, 0);
 
     int size = euler_tour_vertices.size() * 2;
 
-    /*segment_tree.assign(size, seg);
+    segment_tree.assign(size, seg);
 
     cout << "Nodes: ";
     for (int vertex : euler_tour_vertices) {
@@ -116,9 +132,6 @@ int main(){
         for (int j=0; j < tree[i].size(); j++)
         cout << tree[i][j] << " ";
         cout << endl;
-    }
-    for (int i=0; i < card_order.size(); i++) {
-        printf("Vertex: %d  height: %d \n", card_order.at(i), height.at(i));
     }
     */
 
