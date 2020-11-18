0bc6716b60e3ad18c9afeb680dc07302c2ca229c
Junior V. Ramisch <junior.ramisch@gmail.com>
2020-11-17 21:24:56 -0300

    refactor(trab): removes unused registers
    also documents some stuff, accordingly to my tests
    everything remains unchanged

--- semester-v/org/trabs/trab_1.asm
+++ semester-v/org/trabs/trab_1.asm
@@ -36,22 +36,23 @@
 .text
 	main:
 		la a2, operationcodes       # Carrega o vetor dos códigos de operação
-		lw a3, 0(a2)                # Codigo para inserir
+		lw a3, 0(a2)                # Código para inserir
 		lw a4, 4(a2)                # Remover por índice
 		lw a5, 8(a2)                # Remover por valor
 		lw a6, 12(a2)               # Listar valores
 		
-		addi s1, sp, 0                 # Salva o endereço do 1° inserido
-		addi s2, sp, 0
+		addi s1, sp, 0              # Salva o endereço do 1° elemento inserido
+		addi s2, sp, 0              # Salva o endereço do último elemento da lista,
+		                            # na 1ª inserção  o 1º elemento inserido também é o último.
 		
-		addi s3, zero, 0            # Tamanho da lista
+		addi s3, zero, 0            # Armazena o tamanho da lista
 		addi s4, zero, 0            # Quantidade de elemetos adicionados
 		addi s5, zero, 0            # Quantidade de elemetos removidos
 		addi s6, zero, 0            # Salva o tamanho da lista -1
 		
 		j print_menu
 		
-	print_menu:
+	print_menu:                         
 	
 		la a0, header
 		li a7, 4             
@@ -83,15 +84,17 @@
 	input_code:		
 		la a0, inputcode
 		li a7, 4
-		ecall		            # Mostra a mensagem que pede o código
+		ecall		            # Mostra a mensagem que pede o código da operação
 		  
 		li a7, 5
 		ecall		            # Lê o código da operação                        
 		
-		addi t1, s1, 0              # Reseta o endereço do 1º elemento para percorrer a lista
-		addi t2, zero, 0            # Reseta contador para percorrer a lista no remove por indice
+		addi t1, s1, 0              # Reseta o valor de t1 que é o endereço do 1º elemento
+					    # para posteriormente percorrer a lista.
+		addi t2, zero, 0            # Reseta contador para percorrer a lista no remover por índice e 
+					    # remover por valor.
 		
-		beqz a0, terminate
+		beqz a0, terminate          # Caso a0 == 0 termina o programa
 		beq a3, a0, insert_value
 		beq a4, a0, remove_by_index
 		beq a5, a0, remove_by_value
@@ -105,17 +108,16 @@
 		li a7, 5
 		ecall		               # Lê o valor a ser inserido
 		
-		bgt s3, zero, insert_sorted    # Faz o desvio caso o tamanho da lista não for 0 
+		bgt s3, zero, insert_sorted    # Faz o desvio caso o tamanho da lista não for 0,
+					       # caso contrário insere o 1º elemento 
 		
-		# Caso trivial: Insere o primeiro valor
-		addi s1, s2, 0
+		addi s1, s2, 0                 # Caso trivial: Insere o primeiro valor
 
 		sw a0, 0(s2)                   # Coloca o 1° valor na 1ª posição
 		sw zero, -4(s2)                # Zera o endereço para o próximo valor
-		addi a1, s1, 0                 # Salva o endereço para ser utilizado como penultimo elemento na prox inserção
-		addi a2, a1, 0                 # Como as inserções a2 irá se tornar o antepenultimo elemento
 		
-		j update_insert_stats
+		j update_insert_stats          # Atualiza o tamanho da lista e a quantidade de elementos
+		                               # inseridos.
 
 	update_insert_stats:
 		
@@ -126,8 +128,6 @@
 		j input_code
 	
 	insert_sorted:
-		# addi sp, sp, -8                # Alloca memória na pilha
-		# addi s2, sp, 0                 # Atualiza o valor do último elemento
 		
 		lw t5, 0(t1)                     # Carrega o valor da 1ª posição
 		ble a0, t5, insert_element       # Caso o valor inserido seja menor ou igual que o atual então insere 
@@ -135,35 +135,23 @@
 		beq t2, s3, input_code           # Chegou no fim da lista (just in case se loopar infinitamente)
 		
 		addi t2, t2, 1                   # Contador pra controlar onde está sendo inserido (início, meio ou fim)
-		addi t3, t1, 0                   # Guarda o endereço do elemento atual que está comparado
+		addi t3, t1, 0                   # Guarda o endereço do elemento atual que está sendo comparado para posteriormente
+		                                 # usá-lo para ajustar os endereços (na prox iteração t3 será o elemento anterior).
 		lw t1, -4(t1)                    # Carrega o endereço do próximo elemento
 		
 		j insert_sorted
-		# sw a0, 0(s2)                   # Armazena o novo valor
-		# sw zero, -4(s2)                # Coloca zero no endereço do próximo valor
-
-		# sw s2, -4(a1)                  # Atualiza o endereço do último valor
-		# addi a1, s2, 0                 # Salva o endereço do último elemento
-		# addi a2, a1, 0                 # Como as inserções a2 irá se tornar o antepenultimo elemento
-		
-		# j update_insert_stats          # Atualiza os contadores de inserção				
-	
 	
 	insert_element:
 		beqz t2, push_front              # Se o contador for 0 significa que deve ser inserido no início
 			
-		# a0 : novo valor
-		# t3 : anterior
-		# t1 : proximo
-			
-		addi sp, sp, -8
+		addi sp, sp, -8                  # Aloca memória na pilha
 		sw a0, 0(sp)                     # Armazena o novo valor
 		sw t1, -4(sp)                    # Faz o link com o próximo elemento
 		
-		sw sp, -4(t3)                    # Faz o link do elemento anterior com o endereço do novo valor
+		sw sp, -4(t3)                    # Faz o link entre o elemento anterior com o endereço do novo valor (ver linha 138)
 		
 		j update_insert_stats
-		## Insere no meio
+		
 	push_front:
 		addi sp, sp, -8                  # Alloca espaço na pilha
 		sw a0, 0(sp)                     # Armazena o novo valor 
@@ -179,24 +167,24 @@
 		sw a0, 0(sp)                     # Armazena o novo valor  
 		sw zero, -4(sp)                  # Coloca zero no endereço para o próximo valor   
 		
-		sw sp, -4(s2)                    # Armazena o endereço do valor inserido no penúltimo elemento
+		sw sp, -4(s2)                    # Armazena o endereço do valor inserido no penúltimo elemento (que nessa
+		                                 # situação é representado pelo registrador s2).
 		addi s2, sp, 0                   # Atualiza o endereço do último valor
-		addi a1, s2, 0
 		
 		j update_insert_stats	
 	
 	remove_by_index:
-		beqz s3, empty_list            # Desvia caso a lista for vazia
+		beqz s3, empty_list              # Desvia caso a lista for vazia (s3 == 0)
 		
 		la a0, removeindex
 		li a7, 4
-		ecall                          # Pede o indíce que será removido
+		ecall                            # Pede o indíce que será removido
 		
 		li a7, 5
-		ecall                          # Lê o indice
+		ecall                            # Lê o indice
 		
-		bge a0, s3, index_not_found
-		beqz a0, remove_first
+		bge a0, s3, index_not_found      # Desvia caso o índice é >= que o tamanho da lista
+		beqz a0, remove_first            # 
 		j remove_middle 
 	
 	index_not_found:
@@ -208,50 +196,48 @@
 	
 	update_remove_stats:
 	
-		addi s3, s3, -1                # Tamanho da lista
-		addi s6, s3, -1
-		addi s5, s5, 1                 # Elementos removidos
+		addi s3, s3, -1                 # Decrementa o tamanho da lista
+		addi s6, s3, -1                 # Decrementa o tamanho-1
+		addi s5, s5, 1                  # Incrementa a quantidade de elementos removidos
 		j input_code
 	
 	remove_first:
-		lw s1, -4(s1)                  # Define como 1° elemento o endereço do próximo elemento
+		lw s1, -4(s1)                   # Define como 1° elemento o endereço do próximo elemento
  	
 		j update_remove_stats
 	
 	remove_middle:
 		beq t2, a0, change_pointers
-		addi t2, t2, 1                # Incrementa o contador caso o índice não tiver sido encontrado
-		addi t3, t1, 0                # Armazena o endereço do valor anterior
-		addi t4, t3, 0                # Armazena o antepenultimo valor em relação ao indice removido 
-		lw t1, -4(t1)                 # Acessa o endereço do próximo elemento
+		addi t2, t2, 1                  # Incrementa o contador caso o índice não tiver sido encontrado
+		addi t3, t1, 0                  # Armazena o endereço do valor atual que na proxima iteração será o elemento anterior
+		lw t1, -4(t1)                   # Acessa o endereço do próximo elemento
 		
 		j remove_middle	
 	
 	
 	change_pointers:
 						
-		lw t0, -4(t1)                # Carrega o endereço do próximo elemento
-		sw t0, -4(t3)                # Altera o endereço do elemento anterior ao que será excluído (ver linha 161)
+		lw t0, -4(t1)                    # Carrega o endereço do próximo elemento
+		sw t0, -4(t3)                    # Altera o endereço do elemento anterior ao que será excluído (ver linha 161)
 				
-		addi t0, s3, -1
- 		beq t2, t0, update_last_pointer
+ 		beq t2, s6, update_last_pointer  # Caso o valor removido for o último será preciso atualizar
+ 						 # o registrador que aponta para o último elemento
 		j update_remove_stats
 	
 	update_last_pointer:
-		addi a1, t3, 0
 		addi s2, t3, 0
 		j update_remove_stats
 	
 	remove_by_value:
 	
-		beqz s3, empty_list            # Desvia caso a lista for vazia
+		beqz s3, empty_list              # Desvia caso a lista for vazia
 			
 		la a0, removevalue
 		li a7, 4
-		ecall                          # Pede o indíce que será removido
+		ecall                            # Pede o indíce que será removido
 		
 		li a7, 5
-		ecall                          # Lê o índice
+		ecall                            # Lê o índice
 
 		lw t0, 0(t1)
 		beq a0, t0, remove_first
@@ -261,12 +247,10 @@
 	remove_middle_value:
 		
 		beq a0, t0, change_value_pointers
-		bge t2, s6, value_not_found    # Se o contador for maior que o tamanho da lista o valor inserido não existe
+		bge t2, s6, value_not_found      # Se o contador for maior que o tamanho da lista-1 o valor inserido não existe
 
-		
-		addi t2, t2, 1                 # Incrementa o contador caso o valor não tiver sido encontrado
-		addi t3, t1, 0                 # Armazena o endereço do valor anterior
-		addi t4, t3, 0                 # Armazena o antepenultimo valor em relação ao indice removido 
+		addi t2, t2, 1                   # Incrementa o contador caso o valor não tiver sido encontrado
+		addi t3, t1, 0                   # Armazena o endereço do valor anterior
 
 		lw t1, -4(t1)
 		lw t0, 0(t1)
@@ -275,11 +259,9 @@
 	
 	change_value_pointers:
 		lw t0, -4(t1)                   # Carrega o endereço do próximo elemento
-		sw t0, -4(t3)                   # Altera o endereço do elemento anterior ao que será excluído (ver linha 161)
+		sw t0, -4(t3)                   # Altera o endereço do elemento anterior ao que será excluído (ver linha 254)
 				
-		lw t0, 0(s2)
-		lw t5, 0(t1)                    # Acessa o ultimo valor da lista 
- 		beq t5, t0, update_last_pointer # Verifica se o elemento que se deseja remover é o último
+ 		beq t2, s6, update_last_pointer # Verifica se o elemento que se deseja remover é o último
 		j update_remove_stats
 	
 	value_not_found:
@@ -333,7 +315,6 @@
 	empty_list:
 		addi s2, sp, 0                 # Reseta o endereço do 1° e último elemento
 		addi s1, sp, 0
-		addi a1, zero, 0
 		
 		la a0, emptylistmessage
 		li a7, 4
