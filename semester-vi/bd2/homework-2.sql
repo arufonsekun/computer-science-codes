cf4c46e2512e1e0bdb21fcb82d92d19dcd8877d4
Junior V. Ramisch <junior.ramisch@gmail.com>
2021-03-09 01:00:13 -0300

    miau(bd2): homework 2 done?

--- semester-vi/bd2/homework-2.sql
+++ semester-vi/bd2/homework-2.sql
@@ -6,15 +6,21 @@
 -- (não tem mais o produto) então o mesmo deve ser
 -- removido das tabelas Product e Stock.
 
--- INSERT INTO product (eid, description) VALUES (1, 'Banana');
--- INSERT INTO product (eid, description) VALUES (2, 'Apple');
--- INSERT INTO product (eid, description) VALUES (3, 'Orange');
--- INSERT INTO product (eid, description) VALUES (4, 'Mango');
+CREATE OR REPLACE FUNCTION populate_product_and_stock() RETURNS VOID AS $$
+    BEGIN
+        INSERT INTO product (eid, description) VALUES (1, 'Amoxicillin');
+        INSERT INTO product (eid, description) VALUES (2, 'Azithromycin');
+        INSERT INTO product (eid, description) VALUES (3, 'Clonazepam');
+        INSERT INTO product (eid, description) VALUES (4, 'Ibuprofen');
+        INSERT INTO product (eid, description) VALUES (5, 'Relaxon');
 
---INSERT INTO stock (eid, quantity) VALUES (1, 100);
---INSERT INTO stock (eid, quantity) VALUES (2, 150);
---INSERT INTO stock (eid, quantity) VALUES (3, 200);
---INSERT INTO stock (eid, quantity) VALUES (4, 60);
+        INSERT INTO stock (eid, quantity) VALUES (1, 100);
+        INSERT INTO stock (eid, quantity) VALUES (2, 150);
+        INSERT INTO stock (eid, quantity) VALUES (3, 200);
+        INSERT INTO stock (eid, quantity) VALUES (4, 90);
+        INSERT INTO stock (eid, quantity) VALUES (5, 160);
+    END;
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION drop_product() RETURNS TRIGGER AS $$
     BEGIN
@@ -29,21 +35,26 @@ $$ LANGUAGE plpgsql;
 CREATE TRIGGER drop_product_if_out_of_supply 
     AFTER UPDATE
 ON Stock
-    FOR EACH ROW EXECUTE PROCEDURE drop_product()
+    FOR EACH ROW EXECUTE PROCEDURE drop_product();
 
--- DROP FUNCTION drop_product() CASCADE;
--- update stock set quantity=0 where eid=3;
--- update stock set quantity=0 where eid=1;
+select populate_product_and_stock();
+
+update stock set quantity=0 where eid=3;
+update stock set quantity=0 where eid=1;
+
+select * from stock;
+
+drop function populate_product_and_stock();
+drop function drop_product() CASCADE;
 
 
 -- 2.2- Para evitar problemas com uso indevido de medicamentos
 -- controlados, crie uma tabela auxiliar de  controle para verificar
 -- quem está requisitando cada medicamento com a data e hora. Ou seja,
 -- usuário que solicitou (logou no sistema), hora, medicamento_id.
-
 CREATE TABLE medicine_request_log(
     id SERIAL,
-    did INTEGER NOT NULL,
+    current_usr varchar(20),
     date_request timestamp,
     medicine_id INTEGER NOT NULL,
     CONSTRAINT pk_medicine_request_log PRIMARY KEY (id)
@@ -51,7 +62,13 @@ CREATE TABLE medicine_request_log(
 
 CREATE OR REPLACE FUNCTION log_medicine_requests() RETURNS TRIGGER AS $$
     BEGIN
-        INSERT INTO medicine_request_log (did, date_request, medicine_id) VALUES
-        (NEW.did, current_timestamp, NEW.)
+        INSERT INTO medicine_request_log (current_usr, date_request, medicine_id) VALUES
+        (current_user, current_timestamp, NEW.eid);
+        RETURN NEW;
     END;
 $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER log_medicine_access
+    BEFORE UPDATE
+ON Stock
+    FOR EACH ROW EXECUTE PROCEDURE log_medicine_requests();
