958ed76b8e015a915c069cd2875be2fafa309259
arufonsekun <junior.ramisch@gmail.com>
2022-06-01 01:23:03 -0300

    feat: Adds adjacency list for Western Sahara

--- semester-viii/heuristic-search-topics-xv/read-problem-instances.py
+++ semester-viii/heuristic-search-topics-xv/read-problem-instances.py
@@ -1,14 +1,35 @@
 import os
 import numpy as np
 
-def get_file_list(path):
+def get_file_list(path) -> list:
+    """
+    Generates a list of file names given a path.
+
+    This function lists all files given a dir path
+    and returs a list of file names.
+
+    Parameters
+    ----------
+    path : str
+        A directory path
+    
+    Returns
+    -------
+    list
+        A list of file names.
+
+    Example
+    -------
+    >>> get_file_list("./problem-instances")
+    ["ar9152.tsp", "bm33708.tsp", ..., "zi929.tsp"]
+    """
     with os.popen(f"ls { path }") as problem_files:
         files = problem_files.readlines()
         return [f.rstrip() for f in files]
 
 
-def read_problem_instance(problem_file_name):
-    with open(f"./problem-instances/{ problem_file_name }") as problem_file:
+def read_problem_instance(path, problem_file_name) -> list:
+    with open(f"{path}{ problem_file_name }") as problem_file:
         lines = problem_file.readlines()
         total_lines_except_eof = len(lines) - 1
         return [f.rstrip() for f in lines[7:total_lines_except_eof]]
@@ -20,7 +41,7 @@ def compute_adjacency_list(points, problem_file_name):
 
     for point in points:
         index, x1, y1 = point.split(" ")
-        print(f"Computing distances for point {index}")
+        print(f"Computing distances for city {index}")
         for i in range(int(index), len(points)):
             point1 = np.array((float(x1), float(y1)))
             _, x2, y2 = points[i].split(" ")
@@ -30,10 +51,9 @@ def compute_adjacency_list(points, problem_file_name):
 
     with open(f"./adjacency-lists/{problem_file_name}.npy", "wb") as f:
         np.save(f, adjacency_matrix)
-    # print(adjacency_matrix)
 
 if __name__ == "__main__":
     problem_files = get_file_list("./problem-instances/")
     print(problem_files[23])
-    points = read_problem_instance(problem_files[23])
+    points = read_problem_instance("./problem-instances/", problem_files[23])
     compute_adjacency_list(points, problem_files[23])
